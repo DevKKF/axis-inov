be9bbfe898bf3f9256be1413a0f27ebc9bb390cd
DevKKF <koffikf25@gmail.com>
2025-04-06 02:01:36 +0000

    New commit

--- src/configurations/models.py
+++ src/configurations/models.py
@@ -2780,3 +2780,17 @@ class TypeRecours(models.Model):
         verbose_name_plural = "Type de recours sur un sinistre"
 
 
+class Recours(models.Model):
+    code = models.CharField(max_length=100, blank=True, null=True)
+    libelle = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    statut = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.libelle
+
+    class Meta:
+        db_table = 'recours'
+        verbose_name = "Recours sur un sinistre"
+        verbose_name_plural = "Recours sur un sinistre"

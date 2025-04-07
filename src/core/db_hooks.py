34911dc93e86cd571176ed6f8e48fa9e947cea55
DevKKF <koffikf25@gmail.com>
2025-04-07 00:39:49 +0000

    Commit des suppression des fichiers

--- src/core/db_hooks.py
+++
@@ -1,8 +0,0 @@
-from django.db.backends.signals import connection_created
-from django.db import connection
-
-def set_dynamic_row_format(sender, connection, **kwargs):
-    with connection.cursor() as cursor:
-        cursor.execute("SET SESSION innodb_strict_mode=ON;")
-
-connection_created.connect(set_dynamic_row_format)
